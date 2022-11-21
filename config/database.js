5101dba97fdcee145c99df372e81ace8cd0a4d2d
solloc <matthias@maisch.me>
2022-11-21 19:27:51 +0100

    first change after local development

--- config/database.js
+++ config/database.js
@@ -1,13 +1,32 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+
+
+if (process.env.NODE_ENV === 'development') {
+
+  module.exports = ({ env }) => ({
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      client: 'sqlite',
+      connection: {
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      useNullAsDefault: true,
+      debug: false,
     },
-  },
-});
+  });
+  
+} else {
+
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('PGHOST', '127.0.0.1'),
+        port: env.int('PGPORT', 5432),
+        database: env('PGDATABASE', 'strapi'),
+        user: env('PGUSER', 'strapi'),
+        password: env('PGPASSWORD', 'password'),
+        ssl: env.bool(true),
+      },
+    },
+  });
+  
+}
